848f6cb75830d6ed7257587c4719552c1304ac5c
huofangLi <2895844821@qq.com>
2019-04-09 19:10:36 +0800

    huofang commit

--- pages/articles_detail/article_detail.vue
+++ pages/articles_detail/article_detail.vue
@@ -1,8 +1,82 @@
 <template>
-	<div></div>
+	<view class="container">
+		<text class="article-title">{{article.title}}</text>
+		<view class="article-info">
+			<image :src="article.avatar" class="avatar small"></image>
+			<text>{{article.nickname}}</text>
+			<!-- <text class="info-text">{{handleTime(article.createTime)}}</text> -->
+		</view>
+		
+		<view class="grace-text">
+			<rich-text :nodes="article.content" bindtap="tap"></rich-text>
+		</view>
+		<text class="info-text">评论{{comments.length}}</text>
+		<view class="comment-item" v-for="(comment,index) in comments" :key="index">
+			<view class="left"><image :src="comment.avatar" class="avatar small"></image></view>
+			<view class="right">
+				<text>{{comment.nickname}}</text>
+				<text>{{comment.content}}</text>
+				<view>
+					<text style="margin-right: 10px;">{{comments.length-index}}楼</text>
+					<!-- <text>{{handleTime(comment.commentTime)}}</text> -->
+				</view>
+			</view>
+		</view>
+		<input class="uni-input comment-box" type="text" placeholder="写下你的评论" v-model="comment" required="required" />
+		<button class="green-btn" >提交</button>
+	</view>
 </template>
-
 <script>
+	export default{
+		data() {
+			return {
+				article: {
+					aId: 0,
+					title: '',
+					content: '',
+					avatar: '',
+					nickname: '',
+					createTime: ''
+				},
+				comments: [],
+				imgs: [],
+				comment: ''
+			};
+		},
+		onLoad:function(option){
+			console.log(option.aId);
+			this.aId = option.aId;
+		},
+		onShow:function(){
+			this.getArticle();
+		},
+		onPullDownRefresh:function(){
+			this.getArticle();
+		},
+		methods: {
+			getArticle:function(){
+				var _this = this;
+				uni.request({
+					url:this.apiServer + '/article/' + this.aId,
+					method: 'GET',
+					header: {'content-type': 'application/x-www-form-urlencoded'},
+					success: res => {
+						console.log(res.data.data.article.title+'======');
+						_this.article.title = res.data.data.article.title;
+						_this.article.content = res.data.data.article.content;
+						_this.article.nickname = res.data.data.article.nickname;
+						_this.article.avatar = res.data.data.article.avatar;
+						_this.article.createTime = res.data.data.article.createTime;
+						_this.comments = res.data.data.comments;
+						_this.imgs = res.data.data.imgs;
+					},
+					complete: function(){
+						uni.uni.stopPullDownRefresh();
+					}
+				});
+			},
+		}
+	}
 </script>
 
 <style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,40 @@
 <template>
-	<view class="container"><view class="article" v-for="(article, index) in "></view></view>
+	<view class="container">
+		<view class="article" v-for="(article, index) in articles" :key="index">
+			<!-- 标题 -->
+			<text class="article-title" @tap="gotoDetail(article.id)">{{ article.title }}</text>
+			<!-- 大于等于三张图片的显示方式 -->
+			<view class="" v-if="article.imgs.length >= 3">
+				<view class="thumbnail-box">
+					<view class="thumbnail-item" v-for="(item, index) in article.imgs" :key="index">
+						<image :src="item.imgs"></image>
+					</view>
+				</view>
+			</view>
+			<!-- 小于三张图片的显示方式 -->
+			<view class="" v-else-if="article.imgs.length >= 1">
+				<view class="text-img-box">
+					<view class="left">
+						<text>{{ article.title }}</text>
+					</view>
+					<view class="right">
+						<image :src="article.imgs[article.imgs.length - 1].imgs"></image>
+					</view>
+				</view>
+			</view>
+			<!-- 没有图片的显示方式 -->
+			<view class="text-box" v-else>
+				<text>{{ article.title }}</text>
+			</view>
+
+			<view class="article-info">
+				<image :src="article.avatar" class="avatar small"></image>
+				<text class="info-text">{{ article.nickname }}</text>
+				<text class="info-text">{{ handleTime(article.createTime) }}</text>
+			</view>
+			<view class="article-bottom"></view>
+		</view>
+	</view>
 </template>
 
 <script>
@@ -12,54 +47,89 @@ export default {
 	onLoad: function() {
 		this.getArticles();
 	},
-	onShow:function(){},
-	onPullDownRefresh:function() {
+	onShow: function() {},
+	onPullDownRefresh: function() {
 		this.getArticles();
 	},
 	methods: {
-		getArticles:function(){
+		getArticles: function() {
 			var _this = this;
 			uni.request({
-				url:this.apiServer+'/article/list',
-				method:'GET',
-				header:{
+				url: this.apiServer + '/article/list',
+				method: 'GET',
+				header: {
 					'content-type': 'application/x-www-form-urlencoded'
 				},
-				success:res=>{
+				success: res => {
 					_this.articles = res.data.data;
 				},
-				complete:function(){
+				complete: function() {
 					uni.stopPullDownRefresh();
 				}
 			});
 		},
-		gotoDetail:function(aId){
+		gotoDetail: function(aId) {
 			uni.navigateTo({
-				url:'../articles_detail/article_detail'
-			})
+				url: '../articles_detail/article_detail?aId=' + aId
+			});
 		},
-		handleTime:function(data){
-
+		handleTime: function(createTime) {
+			var date = new Date(createTime);
+			var year = date.getFullYear();
+			var month =
+				date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+			var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+			var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+			var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+			var seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+			// 拼接
+			return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
 		},
-		handleContent:function(msg){
-
-		}
+		handleContent: function(msg) {}
 	}
 };
 </script>
 
 <style>
-.content {
-	text-align: center;
-	height: 400upx;
+.container {
+	margin-top: 20px;
 }
-.logo {
-	height: 200upx;
-	width: 200upx;
-	margin-top: 200upx;
+
+.article-title {
+	margin-top: 10px;
 }
-.title {
-	font-size: 36upx;
-	color: #8f8f94;
+
+.text-img-box {
+	display: flex;
+	height: 150px;
+}
+.left {
+	width: 70%;
+}
+.right {
+	width: 30%;
+}
+
+.right image {
+	width: 100%;
+	height: 100%;
+}
+
+.article-bottom {
+	height: 10px;
+	width: 120%;
+	margin-left: -20px;
+	background-color: #dddddd;
+}
+
+.article-info {
+	display: flex;
+	/* justify-content:center; */
+	align-items: center;
+	margin-bottom: 10px;
+}
+
+.info-text {
+	margin-left: 10px;
 }
 </style>
